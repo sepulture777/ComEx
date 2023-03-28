7cc2e43b1a366624aa176507e6d45e2c02698508
Erich <ic19b071@technikum-wien.at>
2023-03-28 12:56:38 +0200

    Initial commit

---
+++ src/main/java/com/example/comex/ComExApplication.java
@@ -0,0 +1,13 @@
+package com.example.comex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comex/ComExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
